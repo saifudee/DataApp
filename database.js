0deb213c048319f6bb207c76aeafc44e366cd348
Saifudee <saifudeen0323@gmail.com>
2023-11-28 16:27:20 +0530

    first commit

---
+++ database.js
@@ -0,0 +1,32 @@
+let data = [
+    {
+      id: 1,
+      name: "Guy Hawkins",
+      age: 27,
+      email:"xyz@gmail.com",
+      country: "Haiti",
+    },
+    {
+      id: 2,
+      name: "Jane Cooper",
+      age: 19,
+      email:"xyzw@gmail.com",
+      country: "Monaco",
+    },
+    {
+      id: 3,
+      name: "Jacob Jones",
+      age: 32,
+      email:"xz@gmail.com",
+      country: "Poland",
+    },
+    {
+      id: 4,
+      name: "Cody Fishers",
+      age: 29,
+      email:"xy@gmail.com",
+      country: "Mexico",
+    }, 
+  ];
+  
+  export default data;
---
+++ index.js
@@ -0,0 +1,68 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import database from "./database.js";
+
+const app = express();
+
+/* To handle the HTTP Methods Body Parser is used, Generally used to extract the entire body portion of an incoming request stream and exposes it on req.body
+ */
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json(database);
+});
+
+app.put("/add", (req, res) => {
+    const { name, age,email, country } = req.body;
+
+    if (database.length === 0) {
+        database.push({
+          id: 1,
+          name,
+          age,
+          email,
+          country,
+        });
+      } else {
+        database.push({
+          id: database[database.length - 1].id + 1,
+          name,
+          age,
+          email,
+          country,
+        });
+      }
+  try {
+    res.status(200).json(database);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+app.delete("/remove/:id", (req, res) => {
+    const idToRemove = parseInt(req.params.id);
+    const indexToRemove = database.findIndex(item => item.id === idToRemove);
+      if (indexToRemove !== -1) {
+      database.splice(indexToRemove, 1);
+      try {
+        res.status(200).json(database);
+      } catch (error) {
+        res.status(404).json({ message: error.message });
+      }
+    } else {
+      res.status(404).json({ message: "Item not found" });
+    }
+  });
+  
+let port = 5000;
+
+app.listen(port, () => {
+  console.log(`Port is running at ${port}`);
+});
